a71ab355f291ffc3fecbe82993164d8a66641d72
302620548 <302620548@student.rocmondriaan.nl>
2021-10-13 11:30:13 +0200

    Categories af. je moet database healthone hebben

--- Modules/Categories.php
+++ Modules/Categories.php
@@ -3,12 +3,18 @@
 function getCategories()
 {
     global $pdo;
-    $categories = $pdo->query('SELECT * FROM category')-> fetchAll(PDO::FETCH_CLASS, 'Category');
+    $categories =$pdo->query('Select * FROM category')->fetchAll( PDO::FETCH_CLASS,'Category');
     return $categories;
-
 }
 
 function getCategoryName(int $id)
 {
-    
+    global $pdo;
+    $sth = $pdo->prepare('Select * FROM category WHERE id=?');
+    $sth-> bindParam(1,$id,PDO::PARAM_INT);
+    $sth->execute();
+    $category=$sth->fetch(PDO::FETCH_ASSOC);
+    return $category['name'];
+
+
 }
--- Templates/categories.php
+++ Templates/categories.php
@@ -15,30 +15,35 @@ include_once('defaults/head.php');
 
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/home">Home</a></li>
+            <li class="breadcrumb-item"><a href="/home">sportcenter</a></li>
             <li class="breadcrumb-item"><a href="/categories">Categories</a></li>
         </ol>
     </nav>
 
-    <?php foreach ($categories as $category): ?>
-    <div class="col-sm-4 col-sm-3">
-        <div class="card">
-            <div class="card-body text-center">
-                <a href="/categories/<?= $category ->id ?>">
-                    <img src="" alt="">
-                </a>
-            </div>
-        </div>
-    </div>
-    ?>
+</div>
+<div class="row gy-3 ">
 
+    <?php global $categories ?>
+    <?php foreach ($categories as $category):?>
+        <div class="col-sm-4 col-md-3">
+            <div class="card">
+                <div class="card-body text-center">
+                    <a href="/categories/<?= $category->id ?>">
+                        <img class="product-img img-responsive center-block" src='/img/categories/<?= $category->picture ?>'/>
+
+                    </a>
+                    <div class="card-title mb-3"><?= $category->name ?>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    <?php endforeach;?>
     <hr>
     <?php
     include_once('defaults/footer.php');
 
     ?>
-</div>
 
 </body>
 </html>
-
