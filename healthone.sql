fd539a080c77397f18722fe34d1756906c162a4d
302620548 <302620548@stundent.rocmondriaan.nl>
2021-10-28 11:37:58 +0200

    nieuwe database voor reviews

--- healthone.sql
+++ healthone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 14 okt 2021 om 13:50
+-- Gegenereerd op: 28 okt 2021 om 11:29
 -- Serverversie: 10.4.21-MariaDB
 -- PHP-versie: 8.0.10
 
@@ -38,10 +38,35 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `picture`) VALUES
-(1, 'crosstrainer', '/categories/crosstrainer.jpg'),
-(2, 'hometrainer', '/categories/hometrainer.jpg'),
-(3, 'loopband', '/categories/loopband.jpg'),
-(4, 'roeitrainer', '/categories/roeitrainer.jpg');
+(1, 'Crosstrainer', 'categories/crosstrainer.jpg'),
+(2, 'Hometrainer', 'categories/hometrainer.jpg'),
+(3, 'Loopband', 'categories/loopband.jpg'),
+(4, 'Roeitrainer', 'categories/roeitrainer.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `opening_hours`
+--
+
+CREATE TABLE `opening_hours` (
+  `id` int(11) NOT NULL,
+  `day` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `opening_hours`
+--
+
+INSERT INTO `opening_hours` (`id`, `day`, `time`) VALUES
+(5, 'Maandag', '07:00 - 20:00'),
+(6, 'Dinsdag', '08:00 - 20:00'),
+(7, 'Woensdag', '07:00 - 20:00'),
+(8, 'Donderdag', '08:00 - 20:00'),
+(9, 'Vrijdag', '07:00 - 20:30'),
+(10, 'Zaterdag', '08:00 - 17:00'),
+(11, 'Zondag', '08:00 - 13:00');
 
 -- --------------------------------------------------------
 
@@ -62,12 +87,42 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `picture`, `description`, `category_id`) VALUES
-(1, 'Recross', '/categories/crosstrainer/cr1.jpg', 'Best reviewed crosstrainer 1980', 1),
-(2, 'Crosstrainer2', '/categories/crosstrainer/cr2.jpg', 'Best reviewed crosstrainer 1980', 1),
-(3, 'Crosstrainer3', '/categories/crosstrainer/cr3.jpg', 'Yep', 1),
-(4, 'crosstrainer 4', '/categories/crosstrainer/cr4.jpg', 'Allo', 1),
-(5, 'Crosstrainer 5', '/categories/crosstrainer/cr5.jpg', 'Yeah it\'s the 5th crosstrainer', 1),
-(6, 'crosstrainer 6', '/categories/crosstrainer/cr6.jpg', 'Yeah guys, look! It\'s the 6th crosstrainer, who would have guessed it\'s a crosstrainer?', 1);
+(1, 'Crosstrainer 1', 'categories/crosstrainer/cr1.jpg', 'Train op een van de meest effectieve manieren met deze crosstrainer. Met deze crosstrainer train je je gehele lichaam, van je benen en armen tot je core. En dat op een fijne en soepele manier. Met 32 weerstandsniveaus en 18 verschillende trainingsprogramma\'s zet je iedere keer net dat stapje extra.', 1),
+(2, 'Crosstrainer 2', 'categories/crosstrainer/cr2.jpg', 'Train op een van de meest effectieve manieren met deze crosstrainer. Met deze crosstrainer train je je gehele lichaam, van je benen en armen tot je core. En dat op een fijne en soepele manier. Met 32 weerstandsniveaus en 18 verschillende trainingsprogramma\'s zet je iedere keer net dat stapje extra.', 1),
+(3, 'Crosstrainer 3', 'categories/crosstrainer/cr3.jpg', 'Train op een van de meest effectieve manieren met deze crosstrainer. Met deze crosstrainer train je je gehele lichaam, van je benen en armen tot je core. En dat op een fijne en soepele manier. Met 32 weerstandsniveaus en 18 verschillende trainingsprogramma\'s zet je iedere keer net dat stapje extra.', 1),
+(4, 'Crosstrainer 4', 'categories/crosstrainer/cr4.jpg', 'Train op een van de meest effectieve manieren met deze crosstrainer. Met deze crosstrainer train je je gehele lichaam, van je benen en armen tot je core. En dat op een fijne en soepele manier. Met 32 weerstandsniveaus en 18 verschillende trainingsprogramma\'s zet je iedere keer net dat stapje extra.', 1),
+(5, 'Crosstrainer 5', 'categories/crosstrainer/cr5.jpg', 'Train op een van de meest effectieve manieren met deze crosstrainer. Met deze crosstrainer train je je gehele lichaam, van je benen en armen tot je core. En dat op een fijne en soepele manier. Met 32 weerstandsniveaus en 18 verschillende trainingsprogramma\'s zet je iedere keer net dat stapje extra.', 1),
+(6, 'Crosstrainer 6', 'categories/crosstrainer/cr6.jpg', 'Train op een van de meest effectieve manieren met deze crosstrainer. Met deze crosstrainer train je je gehele lichaam, van je benen en armen tot je core. En dat op een fijne en soepele manier. Met 32 weerstandsniveaus en 18 verschillende trainingsprogramma\'s zet je iedere keer net dat stapje extra.', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `review`
+--
+
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `description` text NOT NULL,
+  `stars` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `Role` enum('member','admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -77,12 +132,36 @@ INSERT INTO `product` (`id`, `name`, `picture`, `description`, `category_id`) VA
 -- Indexen voor tabel `category`
 --
 ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexen voor tabel `opening_hours`
+--
+ALTER TABLE `opening_hours`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexen voor tabel `product`
 --
 ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexen voor tabel `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `stars` (`stars`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexen voor tabel `user`
+--
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,10 +175,45 @@ ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT voor een tabel `opening_hours`
+--
+ALTER TABLE `opening_hours`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT voor een tabel `product`
 --
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT voor een tabel `review`
+--
+ALTER TABLE `review`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+--
+-- Beperkingen voor tabel `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
